f7d0c3c79112501d249d0a7af3cb75737e76e2d8
igorchagov <igorchagov@mail.ru>
2022-07-15 18:07:48 +0300

    add slider

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 ///1. ScrollAnimations 
 var $containers = $('[data-animation]:not([data-animation-child]), [data-animation-container]');
 $containers.scrollAnimations();
+/* 3. Swiper slider */
+var interleaveOffset = 0.5;
+var swiper = new Swiper('.testimonials-slider', {
+ speed: 1200,
+ autoplay: {
+  delay: 5000
+ },
+ spaceBetween: 30,
+ loop: true,
+ autoHeight: true,
+ pagination: {
+  el: '.swiper-pagination',
+  clickable: true
+ }
+});
