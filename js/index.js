8752b5ea010396e96c259e7d046beccca35d4651
igorchagov <igorchagov@mail.ru>
2022-07-27 18:07:31 +0300

    added some styles

--- js/index.js
+++ js/index.js
@@ -138,26 +138,26 @@ headroom.init()
 /// /// ///
 /// /// ///
 /* 5. number animation (spincrement) */
-// $(document).ready(function () {
-//   var show = true;
-//   var countbox = ".benefits__number";
-//   $(window).on("scroll load resize", function () {
-//     if (!show) return false; // Отменяем показ анимации, если она уже была выполнена
-//     var w_top = $(window).scrollTop(); // Количество пикселей на которое была прокручена страница
-//     var e_top = $(countbox).offset().top; // Расстояние от блока со счетчиками до верха всего документа
-//     var w_height = $(window).height(); // Высота окна браузера
-//     var d_height = $(document).height(); // Высота всего документа
-//     var e_height = $(countbox).outerHeight(); // Полная высота блока со счетчиками
-//     if (w_top + 500 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
-//       $('.benefits__number').css('opacity', '1');
-//       $('.benefits__number').spincrement({
-//         thousandSeparator: "",
-//         duration: 4000
-//       });
-//       show = false;
-//     }
-//   });
-// });
+ $(document).ready(function () {
+   var show = true;
+   var countbox = ".benefits__number";
+   $(window).on("scroll load resize", function () {
+     if (!show) return false; // Отменяем показ анимации, если она уже была выполнена
+     var w_top = $(window).scrollTop(); // Количество пикселей на которое была прокручена страница
+    var e_top = $(countbox).offset().top; // Расстояние от блока со счетчиками до верха всего документа
+     var w_height = $(window).height(); // Высота окна браузера
+    var d_height = $(document).height(); // Высота всего документа
+    var e_height = $(countbox).outerHeight(); // Полная высота блока со счетчиками
+     if (w_top + 500 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
+      $('.benefits__number').css('opacity', '1');
+       $('.benefits__number').spincrement({
+         thousandSeparator: "",
+         duration: 4000
+       });
+      show = false;
+     }
+   });
+ });
 // // //
 // // //
 /* 6. Progress bar */
